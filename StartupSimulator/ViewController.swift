bd74cc5141e8dba1dc91d8ed6fb8182c69f92a87
johnaceto <john.aceto@rutgers.edu>
2018-12-28 13:28:00 -0500

    Initial Commit

---
+++ StartupSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StartupSimulator
+//
+//  Created by John Aceto on 12/28/18.
+//  Copyright © 2018 John Aceto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
